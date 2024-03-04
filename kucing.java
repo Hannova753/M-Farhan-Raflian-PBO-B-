9dbaf6c41271473a005f3c2dead7f9485652dda0
Muhammad Farhan Raflian N. P <145763402+Hannova753@users.noreply.github.com>
2024-03-04 13:56:02 +0700

    Add files via upload

---
+++ kucing.java
@@ -0,0 +1,11 @@
+public class Kucing {
+    public static void main(String[] args) {
+         String name = "Muhammad Sumbul";
+         String addres;
+
+        public void eat(){
+            System.out.println("makan: "+name);
+
+    }
+    }
+}
