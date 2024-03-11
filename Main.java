c7e6b13ecd7697b522b6cfc902627e576c62ab14
Muhammad Farhan Raflian N. P <145763402+Hannova753@users.noreply.github.com>
2024-03-12 03:24:24 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        Kucing kucing = new Kucing();
+        Mahasiswa data = new Mahasiswa();
+        Scanner input = new Scanner(System.in);
+        System.out.println(data.tampilUniversitas());
+        int choice;
+
+        do {
+            System.out.println("Menu:");
+            System.out.println("1. Tambah data mahasiswa");
+            System.out.println("2. Tampilkan data mahasiswa");
+            System.out.println("3. Keluar");
+            System.out.println("Pilih menu (1-3): ");
+
+            choice = input.nextInt();
+            input.nextLine();
+
+            switch (choice) {
+                case 1:
+                    data.tampilDataMahasiswa();
+                    break;
+                case 2:
+                    data.displayDataMahasiswa();
+                    break;
+                case 3:
+                    System.out.println("Tengkyu");
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid. Silakan pilih lagi.");
+            }
+
+        } while (choice != 3);
     }
+
 }
